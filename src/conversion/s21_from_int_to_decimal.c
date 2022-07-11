32ba46e007e048b1c11bf693c6d7449fb04d334e
Curt Rika <curtrika@il-j5.msk.21-school.ru>
2022-07-11 17:13:17 +0300

    :shirt: refactor: added packages tests and conversion

--- src/conversion/s21_from_int_to_decimal.c
+++ src/conversion/s21_from_int_to_decimal.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "s21_decimal.h"
+#include "s21_conversion.h"
 
 int main() {
     s21_decimal decimal;
-    s21_from_int_to_decimal(1000000000,&decimal);
+    s21_from_int_to_decimal(-1000000000,&decimal);
     printf("value = %64llu\n", decimal.bits64[0]);
 }
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     if (src < 0) dst->bits8[15] = 128; //TODO:
+    printf("%d\n", dst->bits8[15]);
     dst->bits64[0] = (src < 0) ? -1 * src : src;
 }
 
