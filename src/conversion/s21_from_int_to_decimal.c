b9d87f0aaf7b295ca0be0772e884e1b11d9172fd
Worker Corrino <workerco@il-g3.msk.21-school.ru>
2022-07-26 15:34:55 +0300

    :white_check_mark: update(tests): +3 tests and minor changes

--- src/conversion/s21_from_int_to_decimal.c
+++ src/conversion/s21_from_int_to_decimal.c
@@ -7,10 +7,6 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst) {
         status = CONVERTATION_OK;
         if (src < 0) dst->bits[3] = MINUS_SIGN;
         dst->bits[0] = (src < 0) ? -1 * src : src;
-        printf("number = %d\n", src);
-        for (int i = 0; i < 4; i++) {
-            printf("arr%d = %u\n", i, dst->bits[i]);
-        }
     } else {
         status = CONVERTATION_ERROR;
     }
