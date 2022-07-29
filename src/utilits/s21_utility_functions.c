a1d9acf89799639738c9c8cb7da05073853f08e1
Hotblack Helene <hotblack@oa-e6.msk.21-school.ru>
2022-07-29 09:07:26 +0300

    :delivery_truck: two new utility functions

--- src/utilits/s21_utility_functions.c
+++ src/utilits/s21_utility_functions.c
@@ -1,5 +1,3 @@
-// #include <limits.h>
-// #include <stdio.h>
 
 int s21_get_bit(int num, int pos) {
     return num >> pos & 1;
@@ -14,9 +12,3 @@ void print_binary_representation(int num) {
     for (int i = 7; i >= 0; i--)
         printf("%i", s21_get_bit(num, i));
 }
-
-// int main(void) {
-//     int a = INT_MAX - 2;
-//     print_binary_representation(a);
-//     return 0;
-// }
