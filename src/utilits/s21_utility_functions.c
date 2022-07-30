742676f6aaba29925dd313dc17fd1fa6b94ba45d
Hotblack Helene <hotblack@oa-i3.msk.21-school.ru>
2022-07-30 10:39:58 +0300

    :star: done utility convertion func

--- src/utilits/s21_utility_functions.c
+++ src/utilits/s21_utility_functions.c
@@ -50,12 +50,21 @@ s21_decimal s21_convert_alt_to_std(s21_decimal_alt alt) {
     s21_decimal std;
     s21_null_decimal(&std);
     int k = 95;
-    for (int i = 2; i >= 0; i--) {
-        for (int j = 31; j >= 0; j--) {
+    for (int i = 2; i >= 0; i--)
+        for (int j = 31; j >= 0; j--)
             std.bits[i] = (std.bits[i] << 1) + alt.bits[k--];
-            printf("%i ", std.bits[i]);
+    std.bits[3] = alt.sign;
+    std.bits[3] <<= 7;
+    k = 16;
+    for (int i = 4; i >= 0; i--) {
+        std.bits[3] <<= 1;
+        if (alt.exp >= k) {
+            alt.exp -= k;
+            std.bits[3]++;
         }
+        k >>= 1;
     }
+    std.bits[3] <<= 16;
     return std;
 }
 
@@ -64,9 +73,9 @@ s21_decimal s21_convert_alt_to_std(s21_decimal_alt alt) {
 //     std.bits[0] = 33;
 //     std.bits[1] = 127;
 //     std.bits[2] = 1;
-//     std.bits[3] = 0;
+//     std.bits[3] = 196608;
 //     s21_decimal_alt alt = s21_convert_std_to_alt(std);
 //     print_binary_representation_alt(alt);
 //     s21_decimal ret = s21_convert_alt_to_std(alt);
-//     printf("\n%u %u %u", ret.bits[0], ret.bits[1], ret.bits[2]);
+//     printf("\n%u %u %u %u", ret.bits[0], ret.bits[1], ret.bits[2], ret.bits[3]);
 // }
