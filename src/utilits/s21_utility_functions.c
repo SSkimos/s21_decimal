d71558c817d283ad66341645054d659b06ce0a6b
Hotblack Helene <hotblack@oa-i3.msk.21-school.ru>
2022-07-30 12:32:48 +0300

    :pencil: functions explanation

--- src/utilits/s21_utility_functions.c
+++ src/utilits/s21_utility_functions.c
@@ -1,51 +1,81 @@
-#include "s21_structures.h"
-#include <stdbool.h>
-#include <stdio.h>
-#include <limits.h>
-
-bool s21_get_bit(unsigned int num, int pos) {
+// достает нужный бит из инта
+// требуется для опроеделения знака стандартного децимала
+// и вывода стандартного и альтернативного децимала в двоичной форме
+bool s21_get_bit_int(unsigned int num, int pos) {
     return (num >> pos) & 1;
 }
 
-bool s21_get_sign(s21_decimal dec) {
-    return s21_get_bit(dec.bits[3], 31);
+// достает знак из стандартного децимала
+// нужна в преобразовании в альтернативную форму
+bool s21_get_sign_std(s21_decimal dec) {
+    return s21_get_bit_int(dec.bits[3], 31);
 }
 
-int s21_get_exp(s21_decimal dec) {
+// достает положение точки из стандартного децимала
+// нужна в преобразовании в альтернативную форму
+int s21_get_exp_std(s21_decimal dec) {
     return (dec.bits[3] >> 16) % 2147483648;
 }
 
-// this function is used for testing purposes
-void print_binary_representation(int num) {
-    for (int i = 31; i >= 0; i--)
-        printf("%i", s21_get_bit(num, i));
+// распечатка бинарной формы стандартного децимала
+// распечатываются склеенные второй, первый и нулевой бит
+// и через пробел -- третий бит
+void print_binary_representation_std(s21_decimal std) {
+    for (int i = 2; i >= 0; i--) {
+        for (int j = 31; j >= 0; j--)
+            printf("%i", s21_get_bit_int(std.bits[i], j));
+    }
+    printf(" ");
+    for (int j = 31; j >= 0; j--)
+        printf("%i", s21_get_bit_int(std.bits[3], j));
+    printf("\n");
 }
 
-// this function is used for testing purposes
+// распечатка бинарной формы альтернативного децимала
+// распечатываются все биты от старшего к младшему
+// и через пробелы - знак (1 - минус, 0 - плюс)
+// и положение точки
 void print_binary_representation_alt(s21_decimal_alt alt) {
     for (int i = 95; i >= 0; i--)
         printf("%i", alt.bits[i]);
+    printf(" %i %i", alt.sign, alt.exp);
 }
 
+// зануление стандартного децимала
+// просто потому что надо
 void s21_null_decimal(s21_decimal *std) {
-    std -> bits[0] = 0;
-    std -> bits[1] = 0;
-    std -> bits[2] = 0;
-    std -> bits[3] = 0;
+    for (int i = 0; i < 4; i++)
+        std -> bits[i] = 0;
 }
 
+// зануление альтернативного децимала
+// просто потому что надо
+void s21_null_decimal_alt(s21_decimal_alt *alt) {
+    alt -> sign = 0;
+    alt -> exp = 0;
+    for (int i = 0; i < 96; i++)
+        alt -> bits[i] = 0;
+}
+
+// преобразование стандартного децимала в альтернативный
+// протестировано, работает с корректными децималами
+// поломается на децимале со слишком большим положением точки
 s21_decimal_alt s21_convert_std_to_alt(s21_decimal std) {
     s21_decimal_alt alt;
-    alt.sign = s21_get_sign(std);
-    alt.exp = s21_get_exp(std);
+    alt.sign = s21_get_sign_std(std);
+    alt.exp = s21_get_exp_std(std);
     int i = 0;
     for (int j = 0; j < 3; j++) {
         for (int k = 0; k < 32; k++)
-            alt.bits[i++] = s21_get_bit(std.bits[j], k);
+            alt.bits[i++] = s21_get_bit_int(std.bits[j], k);
     }
     return alt;
 }
 
+// преобразование альтернативного децимала
+// протестировано, работает с корректными децималами
+// поломается на децимале со слишком большим положением точки
+// скорее всего, возниконовение такого децимала нужно предусматривать в вычислительных функциях 
 s21_decimal s21_convert_alt_to_std(s21_decimal_alt alt) {
     s21_decimal std;
     s21_null_decimal(&std);
@@ -54,7 +84,7 @@ s21_decimal s21_convert_alt_to_std(s21_decimal_alt alt) {
         for (int j = 31; j >= 0; j--)
             std.bits[i] = (std.bits[i] << 1) + alt.bits[k--];
     std.bits[3] = alt.sign;
-    std.bits[3] <<= 7;
+    std.bits[3] <<= 10;
     k = 16;
     for (int i = 4; i >= 0; i--) {
         std.bits[3] <<= 1;
@@ -73,7 +103,8 @@ s21_decimal s21_convert_alt_to_std(s21_decimal_alt alt) {
 //     std.bits[0] = 33;
 //     std.bits[1] = 127;
 //     std.bits[2] = 1;
-//     std.bits[3] = 196608;
+//     std.bits[3] = 2147942400;
+//     print_binary_representation_std(std);
 //     s21_decimal_alt alt = s21_convert_std_to_alt(std);
 //     print_binary_representation_alt(alt);
 //     s21_decimal ret = s21_convert_alt_to_std(alt);
