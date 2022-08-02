80e49d8962759d916782c015c355a603756a2df0
Hotblack Helene <hotblack@at-m2.msk.21-school.ru>
2022-08-02 09:21:19 +0300

    :star: begin division

--- src/arithmetics/s21_arithmetics.c
+++ src/arithmetics/s21_arithmetics.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-#include "../utilits/s21_structures.h"
+// #include "../utilits/s21_structures.h"
 #include "s21_arithmetics.h"
+
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int return_code;
+    if (value_2.bits[0] == 0 && value_2.bits[1] == 0 && value_2.bits[2] == 0) {
+        return_code = 3;  // деление на ноль
+    } else {
+        // побитовое деление
+        s21_decimal_alt alt_value_1 = s21_convert_std_to_alt(value_1);
+        s21_decimal_alt alt_value_2 = s21_convert_std_to_alt(value_2);
+        s21_decimal_alt alt_result;
+        s21_null_decimal_alt(&alt_result);
+        if (alt_value_1.bits[0] == 0 && alt_value_2.bits[0] == 0) {
+            s21_right_shift(&alt_value_1);
+            s21_right_shift(&alt_value_2);
+        }
+        // я пока что не поняла алгоритма побитового деления на степени двойки
+        // но вот сокращение всего на степени двойки!
+        *result = s21_convert_alt_to_std(alt_result);
+        value_1 = s21_convert_alt_to_std(alt_value_1);
+        value_2 = s21_convert_alt_to_std(alt_value_2);
+    }
+    return return_code;
+}
 
 // вычитание децималов
 // не хватает (закомментировано, записано в комментарии):
