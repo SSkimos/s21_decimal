32242707ca762fa7dc4770c7f713872235c2eda5
artemiikulikov <aapqwhat@gmail.com>
2022-08-07 00:31:02 +0300

    :shirt: refactor(tests): tests division WIP

---
+++ src/tests/comparsion_suite.c
@@ -0,0 +1,93 @@
+#include "../utilits/s21_utility.h"
+#include "s21_decimal_test.h"
+#include "../s21_decimal.h"
+
+START_TEST(is_equal_simple) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(A, &second_decimal);
+    ck_assert_int_eq(1, s21_is_equal(decimal, second_decimal));
+} END_TEST
+
+START_TEST(is_equal_0_decimal) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    ck_assert_int_eq(0, s21_is_equal(decimal, second_decimal));
+
+} END_TEST
+
+START_TEST(is_not_equal) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 5123;
+    int B = 5122;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_not_equal(decimal, second_decimal));
+
+} END_TEST
+
+START_TEST(is_not_equal_SIGN) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = -5123;
+    int B = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_not_equal(decimal, second_decimal));
+} END_TEST
+
+START_TEST(is_greater_stand) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = -5123;
+    int B = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_greater(second_decimal, decimal));
+} END_TEST
+
+START_TEST(is_greater_0) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 0;
+    int B = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(0, s21_is_greater(decimal, second_decimal));
+} END_TEST
+
+Suite* comparsion_suite(void) {
+    Suite* s;
+    TCase* tc_core;
+
+    s = suite_create("equal");
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, is_equal_simple);
+    tcase_add_test(tc_core, is_equal_0_decimal);
+    tcase_add_test(tc_core, is_not_equal);
+    tcase_add_test(tc_core, is_not_equal_SIGN);
+    tcase_add_test(tc_core, is_greater_stand);
+    tcase_add_test(tc_core, is_greater_0);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
