4f99cf2d86914ac0dab3396b67f598638300bd86
artemiikulikov <aapqwhat@gmail.com>
2022-08-08 13:37:15 +0300

    :white_check_mark: update(comparsion): add le & lt tests

--- src/tests/comparsion_suite.c
+++ src/tests/comparsion_suite.c
@@ -21,7 +21,6 @@ START_TEST(is_equal_0_decimal) {
     int A = 5123;
     convertation_result status = s21_from_int_to_decimal(A, &decimal);
     ck_assert_int_eq(0, s21_is_equal(decimal, second_decimal));
-
 } END_TEST
 
 START_TEST(is_not_equal) {
@@ -73,6 +72,54 @@ START_TEST(is_greater_0) {
     ck_assert_int_eq(0, s21_is_greater(decimal, second_decimal));
 } END_TEST
 
+START_TEST(is_less_ne) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 5124;
+    int B = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(0, s21_is_less(decimal, second_decimal));
+} END_TEST
+
+START_TEST(is_less_equal) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int B = 5123;
+    int A = B;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(0, s21_is_less(decimal, second_decimal));
+} END_TEST
+
+START_TEST(is_less_Classic) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int A = 0;
+    int B = 5123;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_less(decimal, second_decimal));
+} END_TEST
+
+START_TEST(is_less_or_equal_Classic) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int B = 5123;
+    int A = B;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_less_or_equal(decimal, second_decimal));
+} END_TEST
+
 Suite* comparsion_suite(void) {
     Suite* s;
     TCase* tc_core;
@@ -86,6 +133,9 @@ Suite* comparsion_suite(void) {
     tcase_add_test(tc_core, is_not_equal_SIGN);
     tcase_add_test(tc_core, is_greater_stand);
     tcase_add_test(tc_core, is_greater_0);
+    tcase_add_test(tc_core, is_less_Classic);
+    tcase_add_test(tc_core, is_less_equal);
+    tcase_add_test(tc_core, is_less_or_equal_Classic);
     suite_add_tcase(s, tc_core);
 
     return s;
