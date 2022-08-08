b20b653f859f1a6199876dcdcb42bf2d2abc3bda
Worker Corrino <workerco@at-m2.msk.21-school.ru>
2022-08-08 21:23:24 +0300

    :white_check_mark: update(test): +1 nan test compare

--- src/tests/comparsion_suite.c
+++ src/tests/comparsion_suite.c
@@ -1,6 +1,7 @@
 #include "../utilits/s21_utility.h"
 #include "s21_decimal_test.h"
 #include "../s21_decimal.h"
+#include <math.h>
 
 START_TEST(is_equal_simple) {
     s21_decimal decimal;
@@ -120,6 +121,18 @@ START_TEST(is_less_or_equal_Classic) {
     ck_assert_int_eq(1, s21_is_less_or_equal(decimal, second_decimal));
 } END_TEST
 
+START_TEST(is_less_or_equal_nan) {
+    s21_decimal decimal;
+    s21_decimal second_decimal;
+    init_decimal(&decimal);
+    init_decimal(&second_decimal);
+    int B = 5123;
+    int A = NAN;
+    convertation_result status = s21_from_int_to_decimal(A, &decimal);
+    convertation_result second_status = s21_from_int_to_decimal(B, &second_decimal);
+    ck_assert_int_eq(1, s21_is_less_or_equal(decimal, second_decimal));
+} END_TEST
+
 Suite* comparsion_suite(void) {
     Suite* s;
     TCase* tc_core;
@@ -136,6 +149,7 @@ Suite* comparsion_suite(void) {
     tcase_add_test(tc_core, is_less_Classic);
     tcase_add_test(tc_core, is_less_equal);
     tcase_add_test(tc_core, is_less_or_equal_Classic);
+    tcase_add_test(tc_core, is_less_or_equal_nan);
     suite_add_tcase(s, tc_core);
 
     return s;
