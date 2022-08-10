d09f0be6413618d5f58006679b750568c6e08e2e
Hotblack Helene <hotblack@at-m2.msk.21-school.ru>
2022-08-10 14:09:30 +0300

    :bug: bug in sign

--- src/arithmetics/s21_sub.c
+++ src/arithmetics/s21_sub.c
@@ -9,20 +9,16 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     s21_decimal_alt alt_result;
     s21_null_decimal_alt(&alt_result);
     if (alt_value_1.sign == alt_value_2.sign) {
-        if (s21_is_greater_or_equal(value_1, value_2) && \
-            alt_value_1.sign == 0 || s21_is_less_or_equal(value_1, value_2) \
-            && alt_value_1.sign == 1) {
-                // нормальные побитовые вычисления
-        return_code = s21_sub_alt(alt_value_1, alt_value_2, &alt_result);
-                // }
+        if (s21_is_greater_or_equal(value_1, value_2)) {
+                return_code = \
+                s21_sub_alt(alt_value_1, alt_value_2, &alt_result);
             } else {
                 alt_value_1.sign = alt_value_1.sign ^ 1;
                 alt_value_2.sign = alt_value_2.sign ^ 1;
-                value_1 = s21_convert_alt_to_std(alt_value_2);
-                value_2 = s21_convert_alt_to_std(alt_value_1);
-                return_code = s21_sub(value_1, value_2, result);
+                return_code = \
+                s21_sub_alt(alt_value_2, alt_value_1, &alt_result);
             }
-        // }  // else приведение к большему
+        alt_result.sign = alt_value_1.sign;
     } else {
         alt_value_2.sign = alt_value_2.sign ^ 1;
         value_2 = s21_convert_alt_to_std(alt_value_2);
