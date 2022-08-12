3f55c20530ec42b9e6bca73fa8eeb75cddb60e2c
Worker Corrino <workerco@il-h1.msk.21-school.ru>
2022-08-12 20:08:55 +0300

    :star: update(conversion): merge

--- src/conversion/s21_from_decimal_to_float.c
+++ src/conversion/s21_from_decimal_to_float.c
@@ -1,18 +1,18 @@
 #include "../utilits/s21_utility.h"
 #include "../s21_decimal.h"
 #include <stdio.h>
+#include <math.h>
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     convertation_result status;
     if (dst) {
         status = CONVERTATION_OK;
-        float result = 0;
-        int exp = 0;
-
-        exp = src.bits[3] & 16711680;
-        printf("exp = %d\n", exp);
-
-        *dst = result;
+        int exp = src.bits[3] & 8355840;
+        exp >>= 15;
+        *dst = (float)(src.bits[0] / pow(10, exp));
+        if (src.bits[3] >> 31 == 1) {
+            *dst = -(*dst);
+        }
     } else {
         status = CONVERTATION_ERROR;
     }
