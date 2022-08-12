d1abfc146f9120fd5cb064e1e15d8892655654bb
Worker Corrino <workerco@il-h1.msk.21-school.ru>
2022-08-12 19:33:22 +0300

    :shirt: update(negate): add negate

---
+++ src/other/s21_negate.c
@@ -0,0 +1,24 @@
+#include "../utilits/s21_utility.h"
+#include "../s21_decimal.h"
+#include <stdio.h>
+
+int s21_negate(s21_decimal value, s21_decimal *res) {
+    operation_result result;
+    if (res) {
+        result = OPERATION_OK;
+        printf("before = ");
+        print_binary_representation_std(value);
+        if (value.bits[3] >> 31) {
+            res->bits[3] & 33423360;
+        } else {
+            int buffer = res->bits[3] & 33423360;
+            res->bits[3] <<= 31;
+            res->bits[3] += buffer;
+        }
+        printf("after = ");
+        print_binary_representation_std(*res);
+    } else {
+        result = 1;
+    }
+    return result;
+}
