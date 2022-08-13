0b5be27c97aa0bdda4ee1e9023ac0801f9a11118
Worker Corrino <workerco@at-m5.msk.21-school.ru>
2022-08-13 15:22:52 +0300

    :rotating_light: refactor(rescale): remove cppcheck warnings

--- src/utilits/s21_rescale.c
+++ src/utilits/s21_rescale.c
@@ -15,10 +15,9 @@ s21_decimal_alt *alt_value_2) {
     s21_null_decimal_alt(&ten);
     ten.bits[1] = 1;
     ten.bits[3] = 1;
-    int exp_dif;
     if (!is_null(*alt_value_1) && !is_null(*alt_value_2)) {
         if (alt_value_1 -> exp > alt_value_2 -> exp) {
-            exp_dif = alt_value_1 -> exp - alt_value_2 -> exp;
+            int exp_dif = alt_value_1 -> exp - alt_value_2 -> exp;
             for (int i = 0; i < exp_dif; i++)
                 s21_mul_alt(*alt_value_2, ten, alt_value_2);
             if (last_bit(*alt_value_2) > 95) {
@@ -37,7 +36,7 @@ s21_decimal_alt *alt_value_2) {
 
 void s21_rescale_alt_to_smaller(s21_decimal_alt *alt_value_1, \
 s21_decimal_alt *alt_value_2) {
-    int mod1, mod2;
+    int mod1 = 0, mod2 = 0;
     while (last_bit(*alt_value_2) > 95) {
         mod1 = div_by_ten(alt_value_1);
         mod2 = div_by_ten(alt_value_2);
