8bc3051efdb5b6cd0a532560647b48f44775bcd4
Worker Corrino <workerco@at-m5.msk.21-school.ru>
2022-08-13 15:18:58 +0300

    :shirt: revert(dec_to_float): revert commit

--- src/conversion/s21_from_decimal_to_float.c
+++ src/conversion/s21_from_decimal_to_float.c
@@ -7,7 +7,8 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     convertation_result status;
     if (dst) {
         status = CONVERTATION_OK;
-        int exp = s21_get_exp_std(src);
+        int exp = src.bits[3] & 8355840;
+        exp >>= 15;
         *dst = (float)(src.bits[0] / pow(10, exp));
         if (src.bits[3] >> 31 == 1) {
             *dst = -(*dst);
