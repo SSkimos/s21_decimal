15e35581e7f2c9ea1162ce76cc58e2ca216f7e1b
Worker Corrino <workerco@at-m5.msk.21-school.ru>
2022-08-13 14:28:31 +0300

    :white_check_mark: update(other): merge other func

--- src/other/s21_negate.c
+++ src/other/s21_negate.c
@@ -4,20 +4,21 @@
 
 int s21_negate(s21_decimal value, s21_decimal *res) {
     operation_result result;
-    print_binary_representation_std(value);
     if (1) {
         result = OPERATION_OK;
-        printf("before = ");
-        print_binary_representation_std(value);
         if (value.bits[3] >> 31) {
-           // res->bits[3] & 33423360;
+            res->bits[0] = value.bits[0];
+            res->bits[1] = value.bits[1];
+            res->bits[2] = value.bits[2];
+            res->bits[3] = value.bits[3];
+            res->bits[3] = res->bits[3] + (0 << 31);
         } else {
-            int buffer = res->bits[3] & 33423360;
-            res->bits[3] <<= 31;
-            res->bits[3] += buffer;
+            res->bits[0] = value.bits[0];
+            res->bits[1] = value.bits[1];
+            res->bits[2] = value.bits[2];
+            res->bits[3] = value.bits[3];
+            res->bits[3] = res->bits[3] + (1 << 31);
         }
-        printf("after = ");
-        print_binary_representation_std(*res);
     } else {
         result = 1;
     }
