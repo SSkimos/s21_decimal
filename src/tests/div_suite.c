1493adafb0548a4385575007162a4dc43cd830c3
Hotblack Helene <hotblack@at-k3.msk.21-school.ru>
2022-08-14 13:47:26 +0300

    :white_check_mark: (div) basic division tests

--- src/tests/div_suite.c
+++ src/tests/div_suite.c
@@ -8,44 +8,110 @@ START_TEST(basic_div) {
     s21_decimal ans;
     init_decimal(&dec1);
     init_decimal(&dec2);
-    dec1.bits[0] = 31;
-    // dec1.bits[3] = 393216;
-
-    dec2.bits[0] = 29;
-    // dec2.bits[3] = 393216;
+    dec1.bits[0] = 9;
+    dec1.bits[3] = 65536;
+    dec2.bits[0] = 6;
+    dec2.bits[3] = 65536;
     s21_decimal true_ans;
     init_decimal(&true_ans);
-    true_ans.bits[0] = 774;
-
+    true_ans.bits[0] = 15;
+    true_ans.bits[3] = 65536;
     int status = s21_div(dec1, dec2, &ans);
-    int true_status = 0;  // операция прошла успешно
-    // print_binary_representation_std(dec1);
-    // print_binary_representation_std(dec2);
-    // ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
+    int true_status = 0;
+    ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
     ck_assert_int_eq(status, true_status);
 } END_TEST
 
-// START_TEST(basic_div_2) {
-//     s21_decimal dec1;
-//     s21_decimal dec2;
-//     s21_decimal ans;
-//     init_decimal(&dec1);
-//     init_decimal(&dec2);
-//     dec1.bits[0] = 1732144;
-//     dec1.bits[3] = MINUS_SIGN;
+START_TEST(basic_div_2) {
+    s21_decimal dec1;
+    s21_decimal dec2;
+    s21_decimal ans;
+    init_decimal(&dec1);
+    init_decimal(&dec2);
+    dec1.bits[0] = 14;
+    dec1.bits[3] = 65536;
+    dec2.bits[0] = 9;
+    dec2.bits[3] = 65536;
+    // 14/9 = 1.5555555555 5555555555 55555556
+    // 00110010 01000011 01000001 00000100
+    // 01100000 10101011 01011001 00001000
+    // 11000011 10001110 00111000 11100100
+    // 00000000 00011100 00000000 00000000
+    s21_decimal true_ans;
+    init_decimal(&true_ans);
+    true_ans.bits[0] = 3280877796;
+    true_ans.bits[1] = 1621842184;
+    true_ans.bits[2] = 843268356;
+    true_ans.bits[3] = 1835008;
+    int status = s21_div(dec1, dec2, &ans);
+    int true_status = 0;  // операция прошла успешно
+    ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
+    ck_assert_int_eq(status, true_status);
+} END_TEST
 
-//     dec2.bits[0] = 4;
+START_TEST(basic_div_3) {
+    s21_decimal dec1;
+    s21_decimal dec2;
+    s21_decimal ans;
+    init_decimal(&dec1);
+    init_decimal(&dec2);
+    dec1.bits[0] = 14;
+    dec1.bits[3] = 65536;
+    dec2.bits[0] = 9;
+    dec2.bits[3] = 65536 + MINUS_SIGN;
+    // 14/9 = 1.5555555555 5555555555 55555556
+    // 00110010 01000011 01000001 00000100
+    // 01100000 10101011 01011001 00001000
+    // 11000011 10001110 00111000 11100100
+    // 00000000 00011100 00000000 00000000
+    s21_decimal true_ans;
+    init_decimal(&true_ans);
+    true_ans.bits[0] = 3280877796;
+    true_ans.bits[1] = 1621842184;
+    true_ans.bits[2] = 843268356;
+    true_ans.bits[3] = 1835008 + MINUS_SIGN;
+    int status = s21_div(dec1, dec2, &ans);
+    int true_status = 0;  // операция прошла успешно
+    ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
+    ck_assert_int_eq(status, true_status);
+} END_TEST
 
-//     s21_decimal true_ans;
-//     init_decimal(&true_ans);
-//     true_ans.bits[0] = 433036;
-//     true_ans.bits[3] = MINUS_SIGN;
+START_TEST(div_by_zero) {
+    s21_decimal dec1;
+    s21_decimal dec2;
+    s21_decimal ans;
+    init_decimal(&dec1);
+    init_decimal(&dec2);
+    dec1.bits[0] = 2814903;
+    dec1.bits[3] = 65536;
+    dec2.bits[0] = 0;
+    dec2.bits[3] = 65536 + MINUS_SIGN;
+    int status = s21_div(dec1, dec2, &ans);
+    int true_status = 3;  // не надо делить на ноль
+    ck_assert_int_eq(status, true_status);
+} END_TEST
 
-//     int status = s21_div(dec1, dec2, &ans);
-//     int true_status = 0;  // операция прошла успешно
-//     ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
-//     ck_assert_int_eq(status, true_status);
-// } END_TEST
+START_TEST(div_zero) {
+    s21_decimal dec1;
+    s21_decimal dec2;
+    s21_decimal ans;
+    init_decimal(&dec1);
+    init_decimal(&dec2);
+    dec1.bits[0] = 0;
+    dec1.bits[3] = 65536;
+    dec2.bits[0] = 4578783;
+    s21_decimal true_ans;
+    init_decimal(&true_ans);
+    true_ans.bits[0] = 0;
+    true_ans.bits[1] = 0;
+    true_ans.bits[2] = 0;
+    true_ans.bits[3] = 0;
+    int status = s21_div(dec1, dec2, &ans);
+    int true_status = 0;  // операция прошла успешно
+    ck_assert_int_eq(1, s21_is_equal(ans, true_ans));
+
+    ck_assert_int_eq(status, true_status);
+} END_TEST
 
 Suite* div_suite(void) {
     Suite* s;
@@ -55,7 +121,10 @@ Suite* div_suite(void) {
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, basic_div);
-    // tcase_add_test(tc_core, basic_div_2);
+    tcase_add_test(tc_core, basic_div_2);
+    tcase_add_test(tc_core, basic_div_3);
+    tcase_add_test(tc_core, div_by_zero);
+    tcase_add_test(tc_core, div_zero);
     suite_add_tcase(s, tc_core);
 
     return s;
