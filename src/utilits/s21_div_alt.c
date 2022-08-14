cd38d9936f7840d562be75715458c39e44b8e06d
Hotblack Helene <hotblack@at-k3.msk.21-school.ru>
2022-08-14 09:38:25 +0300

    :shirt: testing division

--- src/utilits/s21_div_alt.c
+++ src/utilits/s21_div_alt.c
@@ -1,14 +1,22 @@
 #include "s21_utility.h"
-
+#include <stdio.h>
 int s21_div_alt(s21_decimal_alt alt_value_1, \
 s21_decimal_alt alt_value_2, s21_decimal_alt *alt_result) {
     int return_code = 0;
     alt_result -> sign = alt_value_1.sign ^ alt_value_2.sign;
     alt_value_1.sign = 0;
     alt_value_2.sign = 0;
+
     s21_rescale_alt_to_zero(&alt_value_1, &alt_value_2);
+    print_binary_representation_alt(alt_value_1);
+    print_binary_representation_alt(alt_value_2);
     s21_decimal_alt result;
     s21_decimal_alt res = div_with_modulo(alt_value_1, alt_value_2, &result);
+    printf("division result:\n");
+    print_binary_representation_alt(result);
+    printf("modulo:\n");
+    print_binary_representation_alt(res);
+
     return return_code;
 }
 
@@ -20,6 +28,8 @@ s21_decimal_alt alt_value_2, s21_decimal_alt *alt_result) {
     s21_null_decimal_alt(alt_result);
     if (compare_bits(alt_value_1, alt_value_2))
         align(&alt_value_1, &alt_value_2);
+    print_binary_representation_alt(alt_value_1);
+    print_binary_representation_alt(alt_value_2);
     for (int i = 0; i < 191; i++) {
         if (compare_bits(alt_value_1, alt_value_2)) {
             s21_sub_alt(alt_value_1, alt_value_2, &alt_value_1);
@@ -30,6 +40,7 @@ s21_decimal_alt alt_value_2, s21_decimal_alt *alt_result) {
         if (last_bit(alt_value_2) > denominator_left_bit)
             s21_right_shift(&alt_value_2);
         s21_left_shift(alt_result);
+        printf("%i ", i);
     }
     return alt_value_1;
 }
