1098aed56f99f7445f21912954756c1130fde250
Hotblack Helene <hotblack@at-k3.msk.21-school.ru>
2022-08-15 21:01:01 +0300

    :shirt: refactor truncate

--- src/other/s21_truncate.c
+++ src/other/s21_truncate.c
@@ -5,8 +5,7 @@ int s21_truncate(s21_decimal value, s21_decimal *res) {
     int return_code = 0;
     if (res) {
         s21_decimal_alt alt_value = s21_convert_std_to_alt(value);
-        while (alt_value.exp > 0)
-            div_by_ten(&alt_value);
+        s21_truncate_alt(&alt_value);
         *res = s21_convert_alt_to_std(alt_value);
     } else {
         return_code = 1;
