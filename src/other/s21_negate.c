1b30a90e500a44e7cdde2e9d42861aaf9c627e06
Hotblack Helene <hotblack@at-k3.msk.21-school.ru>
2022-08-16 10:58:19 +0300

    :white_check_mark: (negate) negate tests

--- src/other/s21_negate.c
+++ src/other/s21_negate.c
@@ -5,19 +5,8 @@ int s21_negate(s21_decimal value, s21_decimal *res) {
     operation_result result;
     if (res) {
         result = OPERATION_OK;
-        if (value.bits[3] >> 31) {
-            res->bits[0] = value.bits[0];
-            res->bits[1] = value.bits[1];
-            res->bits[2] = value.bits[2];
-            res->bits[3] = value.bits[3];
-            res->bits[3] = res->bits[3] + (0 << 31);
-        } else {
-            res->bits[0] = value.bits[0];
-            res->bits[1] = value.bits[1];
-            res->bits[2] = value.bits[2];
-            res->bits[3] = value.bits[3];
-            res->bits[3] = res->bits[3] + (1 << 31);
-        }
+        value.bits[3] ^= MINUS_SIGN;
+        *res = value;
     } else {
         result = 1;
     }
