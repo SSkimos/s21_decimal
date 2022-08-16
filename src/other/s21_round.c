692fdc28cfb81b0b557765b140ed44a205f754d1
Hotblack Helene <hotblack@at-e4.msk.21-school.ru>
2022-08-16 11:53:05 +0300

    :heavy_plus_sign: (round) made round

--- src/other/s21_round.c
+++ src/other/s21_round.c
@@ -4,7 +4,18 @@
 int s21_round(s21_decimal value, s21_decimal *res) {
     int return_code = 0;
     if (res) {
-        *res = value;
+        s21_decimal_alt alt_value = s21_convert_std_to_alt(value);
+        int mod = 0;
+        while (alt_value.exp > 0)
+            mod = div_by_ten(&alt_value);
+        if (mod >= 5) {
+            s21_decimal_alt one;
+            s21_null_decimal_alt(&one);
+            one.bits[0] = 1;
+            one.sign = alt_value.sign;
+            s21_add_alt(one, alt_value, &alt_value);
+        }
+        *res = s21_convert_alt_to_std(alt_value);
     } else {
         return_code = 1;
     }
