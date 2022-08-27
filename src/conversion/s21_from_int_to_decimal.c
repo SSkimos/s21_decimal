c3a7152a68f16a8e393aa62ab25108a139428efe
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-08-27 17:45:57 +0300

    :bug: fix bug int->decimal

--- src/conversion/s21_from_int_to_decimal.c
+++ src/conversion/s21_from_int_to_decimal.c
@@ -1,11 +1,13 @@
 #include "../s21_decimal.h"
+#include "../utilits/s21_utility.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
     convertation_result status = 0;
     if (dst) {
         status = CONVERTATION_OK;
+        init_decimal(dst);
         if (src < 0) dst->bits[3] = MINUS_SIGN;
-        dst->bits[0] = (src < 0) ? -1 * src : src;
+        dst->bits[0] = (src < 0) ? 0 - src : src;
     } else {
         status = CONVERTATION_ERROR;
     }
